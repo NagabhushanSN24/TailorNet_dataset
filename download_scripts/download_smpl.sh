eba3128deb807290caa14ff3cbf0c3f894b30b6b
Nagabhushan S N <nagabhushan@spreeai.com>
2024-06-20 14:31:21 +0000

    Minor changes

--- download_scripts/download_smpl.sh
+++ download_scripts/download_smpl.sh
@@ -8,9 +8,8 @@ read -p "Password (SMPL-X):" password
 username=$(urle $username)
 password=$(urle $password)
 
-mkdir -p ../data/smpl
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smpl&sfile=SMPL_python_v.1.1.0.zip' -O 'data/smpl.zip' --no-check-certificate --continue
-unzip ../data/smpl.zip -d ../data/smpl1
-mv ../data/smpl1/SMPL_python_v.1.1.0/smpl ../data/smpl
-rmdir ../data/smpl1/SMPL_python_v.1.1.0
-rmdir ../data/smpl1
+mkdir -p ../data
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smpl&sfile=SMPL_python_v.1.0.0.zip' -O '../data/smpl_v1.0.0.zip' --no-check-certificate --continue
+unzip ../data/smpl_v1.0.0.zip -d ../data/smpl_v1.0.0
+mv ../data/smpl_v1.0.0/smpl ../data/
+rm -rf ../data/smpl_v1.0.0
--- global_var.py
+++ global_var.py
@@ -2,6 +2,6 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = "TRUE"
 
 PROJ_ROOT = os.path.dirname(os.path.abspath(__file__))
-ROOT = r'D:\data\v3'  # PLEASE replace this by your own data directory!
-SMPL_PATH_MALE = os.path.join(ROOT, 'smpl', 'basicmodel_m_lbs_10_207_0_v1.0.0.pkl')
-SMPL_PATH_FEMALE = os.path.join(ROOT, 'smpl', 'basicModel_f_lbs_10_207_0_v1.0.0.pkl')
+ROOT = r'/mnt/zfs/ml-ab-team/nagabhushan/01_SizeBasedTryOn/workspace/clothing_humans/literature/001_TailorNet/TailorNet_dataset/data'  # PLEASE replace this by your own data directory!
+SMPL_PATH_MALE = os.path.join(ROOT, 'smpl', 'models', 'basicmodel_m_lbs_10_207_0_v1.0.0.pkl')
+SMPL_PATH_FEMALE = os.path.join(ROOT, 'smpl', 'models', 'basicModel_f_lbs_10_207_0_v1.0.0.pkl')
